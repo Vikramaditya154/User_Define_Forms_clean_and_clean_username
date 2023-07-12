b688c88569653f51e63a36716e32d8efecc03e55
Arjunroyal <134121091+Vikramaditya154@users.noreply.github.com>
2023-07-12 22:21:31 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def student_form(request):
     if request.method=='POST':
         SF=StudentForm(request.POST)
         if SF.is_valid():
-            return HttpResponse(str(SFD.cleaned_data))
+            return HttpResponse(str(SF.cleaned_data))
         else:
             return HttpResponse('data is not valid')
     return render(request,'Student_from.html',{'SFD':SFD})
